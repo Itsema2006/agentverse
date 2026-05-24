920dc8ccd2254adf973d0c3b336dbef2dc259a28
Prayuja <prayujajadhav2006@gmail.com>
2026-05-24 21:16:06 +0530

    Fix checkout API base URL

--- js/checkout.js
+++ js/checkout.js
@@ -13,10 +13,6 @@ const Checkout = (function () {
       return window.AGENTVERSE_API_BASE_URL.replace(/\/+$/, '');
     }
 
-    if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
-      return `${window.location.protocol}//${window.location.hostname}:3000`;
-    }
-
     return window.location.origin;
   })();
 
